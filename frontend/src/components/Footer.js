ff015e49baa8b8689cb79bc865d9da26cb818639
Konsta <konsta.laurell@edu.salpaus.fi>
2025-02-03 12:46:35 +0200

    noin

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer>
       <div className="links">
-        <li> <a href='https://taitaja2024.fi/fi/tietoa-sivustosta/'>Tietoa sivustosta</a></li>
+        <li><a href='https://taitaja2024.fi/fi/tietoa-sivustosta/'>Tietoa sivustosta</a></li>
         <li><a href='https://taitaja2024.fi/fi/tietosuojakaytanto/'>Tietosuojakäytäntö</a></li>
         <li><a href='https://taitaja2024.fi/fi/saavutettavuusseloste/'>Saavutettavuusseloste</a></li>
         <li><a href='https://taitaja2024.fi/fi/lajivastaaville/'>Lajivastaaville</a></li>
