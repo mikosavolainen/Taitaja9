2adbaca20b77e7d87e9daaebb751ddd5f2a20465
Konsta <konsta.laurell@edu.salpaus.fi>
2025-02-04 09:31:37 +0200

    Add Etusivu component and update App layout; enhance Header button style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import NotFound from './pages/NotFound';
+import Etusivu from './pages/Etusivu';
 import ServerNotFound from './pages/ServerNotFound';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Etusivu />
       <Footer />      
     </div>
   );
---
+++ frontend/src/pages/Etusivu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Etusivu = () => {
+	return (
+        <div>
+            <div>
+                <h1>TOP 5</h1>
+            </div>
+            <div>
+                <h2>Kisan tiedot</h2>
+            </div>
+        </div>
+	);
+};
+
+export default Etusivu;
