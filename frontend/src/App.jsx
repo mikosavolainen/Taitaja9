dc318d2748f257324bd77209345e76d36685cb18
Konsta <konsta.laurell@edu.salpaus.fi>
2025-02-04 10:48:30 +0200

    Add Infotaulu component and include it in App layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import Header from './components/Header';
 import NotFound from './pages/NotFound';
 import Etusivu from './pages/Etusivu';
 import ServerNotFound from './pages/ServerNotFound';
+import Infotaulu from './pages/Infotaulu';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Infotaulu />
       <Footer />      
     </div>
   );
---
+++ frontend/src/pages/Infotaulu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Infotaulu = () => {
+    return (
+        <div>
+            <h1>Infotaulu Page</h1>
+            <p>Welcome to the Infotaulu page!</p>
+        </div>
+    );
+};
+
+export default Infotaulu;
