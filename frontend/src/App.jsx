bb9d2bb39026b8f3e1de9613eb4a28d0383f688c
Konsta <konsta.laurell@edu.salpaus.fi>
2025-02-07 10:05:32 +0200

    Refactor App component; clean up imports and update routing for Joukkuehallinta page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';//
-import NotFound from './pages/NotFound';//
-import Etusivu from './pages/Etusivu';
-import ServerNotFound from './pages/ServerNotFound';//
-import Infotaulu from './pages/Infotaulu';
-import RastiInput from './pages/RastiInput';//
-import AddTeam from './pages/Addteam';
-import UusiKilpailu from './pages/UusiKilpailu';//
-import VaihdaSalasana from './pages/VaihdaSalasana';//
-import AddUser from './pages/AddUser';//
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header"; //
+import NotFound from "./pages/NotFound"; //
+import Etusivu from "./pages/Etusivu";
+import ServerNotFound from "./pages/ServerNotFound"; //
+import Infotaulu from "./pages/Infotaulu";
+import RastiInput from "./pages/RastiInput"; //
+import AddTeam from "./pages/Addteam";
+import UusiKilpailu from "./pages/UusiKilpailu"; //
+import VaihdaSalasana from "./pages/VaihdaSalasana"; //
+import AddUser from "./pages/AddUser"; //
 import Dashboard from "./pages/Dashboard";
-import { Routes, Route } from 'react-router-dom';
-import Joukkuehallinta from "./pages/JoukkueHallinta"
-import Login from "./pages/Login"
-import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import Joukkuehallinta from "./pages/JoukkueHallinta";
+import Login from "./pages/Login";
+import { useState } from "react";
 function App() {
-	const [logged , Setlogged]= useState(false)
-  return (
+	const [logged, Setlogged] = useState(false);
+	return (
 		<div className="App">
-		  <Header logged={logged} />
+			<Header logged={logged} />
 			<Routes>
 				<Route path="/" element={<Etusivu />} />
 				<Route
@@ -35,11 +35,11 @@ function App() {
 				<Route path="/UusiKilpailu" element={<UusiKilpailu />} />
 				<Route path="/VaihdaSalasana" element={<VaihdaSalasana />} />
 				<Route path="/lisaakayttaja" element={<AddUser />} />
-				<Route path="/logged ? "/dashboard" : "/login"" element={<Joukkuehallinta />} />
+				<Route path="/joukkuehallinta" element={<Joukkuehallinta />} />
 			</Routes>
 			<Footer />
 		</div>
-  );
+	);
 }
 
 export default App;
